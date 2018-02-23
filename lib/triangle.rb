498720f47ff759492e089045bede7b54925c9401
Kaspar JohannesPärtel <kasparjohannes.partel@tptlive.ee>
2018-02-23 16:07:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,9 @@
 class Triangle
+  attr_reader :first_side, :second_side, :third_side
   # write code here
   def initialize(first_side, second_side, third_side)
-    
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
   end
 end
